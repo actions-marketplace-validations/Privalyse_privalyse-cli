5b72c317ab2d2015a8ab6c2ce6bef2af9b2629eb
Samuel <samumatz@gmail.com>
2025-12-25 18:58:30 +0000

    chore: bump version to 0.3.2 for marketplace release

--- privalyse_scanner/__init__.py
+++ privalyse_scanner/__init__.py
@@ -12,7 +12,7 @@ Package Structure:
 - utils: Helper functions and utilities
 """
 
-__version__ = "0.3.1"
+__version__ = "0.3.2"
 __author__ = "Privalyse Team"
 
 from .core.scanner import PrivalyseScanner
